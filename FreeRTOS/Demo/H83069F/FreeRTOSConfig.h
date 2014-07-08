522433dabf3f521fb98e9508cfb550129594741a
Ainehanta <ainehanta@hotmail.co.jp>
2014-07-09 08:50:13 +0900

    Modify Tick Type 32bit to 16bit

--- FreeRTOS/Demo/H83069F/FreeRTOSConfig.h
+++ FreeRTOS/Demo/H83069F/FreeRTOSConfig.h
@@ -91,7 +91,7 @@
 #define configTOTAL_HEAP_SIZE		( ( size_t ) ( 15 * 1024 ) )
 #define configMAX_TASK_NAME_LEN		( 8 )
 #define configUSE_TRACE_FACILITY	0
-#define configUSE_16_BIT_TICKS		0
+#define configUSE_16_BIT_TICKS		1
 #define configIDLE_SHOULD_YIELD		1
 
 /* Co-routine definitions. */
