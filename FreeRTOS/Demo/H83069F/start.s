daeb705871b7d6d814763cc58f74bbb7ae856702
Ainehanta <ainehanta@hotmail.co.jp>
2014-07-09 08:49:49 +0900

    Architecture set H8300H

--- FreeRTOS/Demo/H83069F/start.s
+++ FreeRTOS/Demo/H83069F/start.s
@@ -1,24 +1,4 @@
-
-#ifdef __H8300H__  
-
-#ifdef __NORMAL_MODE__
-	.h8300hn
-#else
 	.h8300h
-#endif
-
-#endif	/*_H8300H_ */
-
-#ifdef __H8300S__
-
-#ifdef __NORMAL_MODE__
-	.h8300sn
-#else
-	.h8300s
-#endif
-
-#endif /* __H8300S__ */
-	
 	.section .text
 	.global _start
 
